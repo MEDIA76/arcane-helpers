19dbf662828948ce639fc437416e8e7c95634507
Joshua Britt <joshua.britt@pm-international.com>
2019-03-24 17:22:37 -0500

    Updated function to support multi-byte.

--- truncate/truncate.php
+++ truncate/truncate.php
@@ -1,12 +1,12 @@
 <?php
 
 /**
- * Truncate 19.02.2 Arcane Helpers
- * https://github.com/MEDIA76/arcane/
+ * Truncate 19.03.1 Arcane Helpers
+ * https://github.com/MEDIA76/arcane
 **/
 
 return function($string, $limit = 100, $suffix = '...') {
-  return substr($string, 0, $limit) . $suffix;
+  return mb_substr($string, 0, $limit) . $suffix;
 }
 
 ?>
