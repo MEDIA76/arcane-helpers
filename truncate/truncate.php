5fcb6507126e565d4eb7a59a1df35fa8c65b43a4
Joshua Britt <joshua.britt@pm-international.com>
2019-03-25 22:35:28 -0500

    Added condition for strings that have not reached limit.

--- truncate/truncate.php
+++ truncate/truncate.php
@@ -1,12 +1,16 @@
 <?php
 
 /**
- * Truncate 19.03.1 Arcane Helpers
+ * Truncate 19.03.2 Arcane Helpers
  * https://github.com/MEDIA76/arcane
 **/
 
 return function($string, $limit = 100, $suffix = '...') {
-  return mb_substr($string, 0, $limit) . $suffix;
+  if(mb_strlen($string) > $limit) {
+    $string = mb_substr($string, 0, $limit) . $suffix;
+  }
+
+  return $string;
 }
 
 ?>
